99fa6162d2bb05c6b11dbc6791dfcc5021f362af
fruitourist <70770455+fruitourist@users.noreply.github.com>
2020-10-24 22:34:34 +0400

    Create problemNullify.cpp

---
+++ problemNullify.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+bool isPrime(int n) {
+	if (n <= 1)
+		return false;
+
+	for (int i = 2; i < n; i++)
+		if (n % i == 0)
+			return false;
+
+	return true;
+}
+
+/*decision vector*/
+
+int enum(int n) {
+  /*decision processing*/
+
+	if (!isPrime(n))
+		/*enum(min b from a * b: 1 < a <= b, s++)*/
+	else
+		enum(n - 1, s++);
+}
+
+//test
+int main() {
+  cout << enum(4) << '\n' << enum(7);
+}
+
+/*int main() {
+	int q;
+	cin >> q;
+
+	int n;
+  vector<int> a; //answer vector
+	for (int i = 0; i < q; i++) {
+		cin >> n;
+		a[i] = enum(n);
+	}
+
+	for (int i = 0; i < q; i++)
+		cout << a[i] << '\n';
+}*/
