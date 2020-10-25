9e5cfb5de3e5317f3209c7b67fba25b15f8b1e95
fruitourist <70770455+fruitourist@users.noreply.github.com>
2020-10-25 10:47:53 +0400

    Update problemNullify.cpp

--- problemNullify.cpp
+++ problemNullify.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <vector>
+/*#include <vector>*/
 
 using namespace std;
 
@@ -14,6 +14,20 @@ bool isPrime(int n) {
 	return true;
 }
 
+int special(int n) {
+
+	if (sqrt(n) == (int)sqrt(n))
+		return sqrt(n);
+
+	int p = 1;
+	for (int m = 2; m < n / 2; m++)
+		if (n % m == 0)
+			if (n / m != n / p)
+				p = n / m;
+			else
+				return p;
+}
+
 /*decision vector*/
 
 int enum(int n) {
@@ -24,12 +38,14 @@ int enum(int n) {
         while (n != 0) {
 
 	    if (!isPrime(n))
-		    n = /*min b from n = a * b: 1 < a <= b)*/
+		    n = special(n); //min b from n = a * b: 1 < a <= b
             else
 		    n--;
             
             s++;
         }
+	
+	return s;
 }
 
 int main() {
