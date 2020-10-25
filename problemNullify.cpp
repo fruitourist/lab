c11108f108069a7663d1d03f555ba9d89b9a9b6d
fruitourist <70770455+fruitourist@users.noreply.github.com>
2020-10-25 11:04:26 +0400

    Update problemNullify.cpp

--- problemNullify.cpp
+++ problemNullify.cpp
@@ -30,7 +30,7 @@ int special(int n) {
 
 /*decision vector*/
 
-int enum(int n) {
+int enumeration(int n) {
   	
         /*decision processing*/
         
@@ -59,5 +59,5 @@ int main() {
 	}
 
 	for (int i = 0; i < q; i++)
-		cout << aswer[i] << '\n';
+		cout << answer[i] << '\n';
 }
