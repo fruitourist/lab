b80a93cc6f8bf2489033700d51931fbf3eb54b58
fruitourist <70770455+fruitourist@users.noreply.github.com>
2020-11-02 12:44:00 +0400

    Update problemHonestCoach.cpp

--- problemHonestCoach.cpp
+++ problemHonestCoach.cpp
@@ -1,43 +1,28 @@
 #include <iostream>
 #include <vector>
-
+#include <algorithm>
+ 
 using namespace std;
-
-int match[2][50];
-int enumeration(vector<int> s, bool command, int position, int n) {
-	for (int i = (int)s.size() - 1; i >= 0; i--) { //for vector specific
-		match[command][position] = s[i];
-		s.pop_back();
-
-		if ((int)s.size() == 0) {
-			/*wighting processing*/
-		}
-
-		//command handling
-		if (position + 1 > n / 2) {
-			command = 1;
-			position = -1;
-		}
-		
-		return enumeration(s, command, position + 1, n);
-	}
-}
-
+ 
 int main() {
-	int t, n;
-	cin >> t;
-	
-	vector<int> s; int sj;
+	int t; cin >> t;
+ 
 	for (int i = 0; i < t; i++) {
-		cin >> n;
+		int n; cin >> n;
 		
-		for (int j = 0; j < n; j++) {
-			cin >> sj;
-			s.push_back(sj);
-		}
-
-		cout << enumeration(s, 0, 0, n);
-
-		s.clear();
+		vector<int> a(n);
+		for (int& j : a)
+			cin >> j;
+		
+		sort(a.begin(), a.end());
+		
+		int r = a[n - 1] - a[0];
+		for (int i = 0; i < n; i++)
+			for (int j = i + 1; j < n; j++)
+				r = min(r, a[j] - a[i]);
+		
+		cout << r << '\n';
 	}
+ 
+	return 0;
 }
